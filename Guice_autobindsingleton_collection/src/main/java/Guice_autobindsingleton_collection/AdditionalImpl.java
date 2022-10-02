eca6f1d5dfc1edff867249fb50473004ed230e54
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2022-10-02 10:46:33 +0200

    Removed unnecessary comment in guice_autobindsingleton

--- Guice_autobindsingleton_collection/src/main/java/Guice_autobindsingleton_collection/AdditionalImpl.java
+++ Guice_autobindsingleton_collection/src/main/java/Guice_autobindsingleton_collection/AdditionalImpl.java
@@ -6,7 +6,7 @@ import javax.inject.Singleton;
 
 @Singleton
 // TODO: uncomment this
-// @AutoBindSingleton(multiple = true, baseClass = SimpleInterface.class)   // this is uncommented
+// @AutoBindSingleton(multiple = true, baseClass = SimpleInterface.class)
 public class AdditionalImpl implements SimpleInterface {
     @Override
     public String getName() {
